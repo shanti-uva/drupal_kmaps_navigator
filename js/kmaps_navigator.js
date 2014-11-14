6fe54a0b36d2e11c3218079ac8dbe39947a8f047
ys2n <ys2n@virginia.edu>
2014-11-14 09:10:01 -0500

    ajax fixes

--- js/kmaps_navigator.js
+++ js/kmaps_navigator.js
@@ -71,24 +71,49 @@ jQuery(function ($) {
             extensions: [ "filter", "glyph"],
             checkbox: false,
             selectMode: 2,
+            theme: 'bootstrap',
             debugLevel: 0,
-            autoScroll: true,
-            // closeOnExternalClick:false,
-            // flapMargin:0,
-            filter: {mode: 'hide'},
+            // autoScroll: true,
+            filter: {
+                mode: "hide",
+                leavesOnly: false
+            },
             activate: function (event, data) {
                 // console.log("activate " + data.node.key);
+
+                event.preventDefault();
 
                 var listitem = $(".title-field[kid='" + data.node.key + "']");
                 $('.row_selected').removeClass('row_selected');
                 $(listitem).closest('tr').addClass('row_selected');
+                $('#ajax-id-' + data.node.key).once('ajax-id-' + data.node.key, function() {
+                    var base = $(this).attr('id');
+                    var argument = $(this).attr('argument');
+
+                    var element_settings = {
+                      url: location.origin + location.pathname.substring(0, location.pathname.lastIndexOf(Settings.type)) + Settings.type + '/' + data.node.key + '/overview/nojs',
+                      event: 'click',
+                      progress: {
+                        type: 'throbber'
+                      }
+                    };
+                    Drupal.ajax[base] = new Drupal.ajax(base, this, element_settings);
+                    $(this).click();
+                });
+
 
 
                 // TODO: CHANGE THIS TO A CONFIGURABLE CALLBACK
                 // CURRENTLY THIS IS A NASTY KLUDGE
-                var loco = ((location.pathname.indexOf('drupal')!= -1)?"/drupal/":"/") + Settings.type  + "/" + data.node.key;
-                console.log ("navigating: " + loco );
+                //var loco = ((location.pathname.indexOf('drupal')!= -1)?"/drupal/":"/") + Settings.type  + "/" + data.node.key;
+                //console.log ("navigating: " + loco );
                 // window.location = loco;
+            },
+            createNode: function (event, data) {
+                console.log(data);
+                var nodeUrl = location.origin + location.pathname.substring(0, location.pathname.lastIndexOf(Settings.type))  + Settings.type + '/' + data.node.key + '/overview/nojs';
+                data.node.span.childNodes[2].innerHTML = '<a href="' + nodeUrl + '" class="use-ajax" id="ajax-id-' + data.node.key + '">' + data.node.title + '</a>';
+                return data;
             },
             glyph: {
                 map: {
